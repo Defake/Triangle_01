1bbd496c7bda3150d538b99cd11201302b179442
Defake <defake@yandex.ru>
2016-09-26 23:54:24 +0500

    Camit

---
+++ Triangle.Test/TriangleTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle.Test {
+
+	[TestClass]
+	public class TriangleTest {
+		
+		/* Here must be at least and only 3 arguments! */
+		private void TestAlgorithm(double[] sides, double[] angles, double expectedArea) {
+			Triangle_01.Triangle tr;
+
+			if (sides.Length == 3)
+				tr = new Triangle_01.Triangle(sides);
+			else if (sides.Length == 2)
+				tr = new Triangle_01.Triangle(sides, angles[0]);
+			else
+				tr = new Triangle_01.Triangle(sides, angles[0], angles[1]);
+
+			Assert.AreEqual(expectedArea, tr.GetArea(), 0.0001);
+
+		}
+
+		[TestMethod]
+		public void ThreeSidesTest() {
+			TestAlgorithm(new double[] { 3, 4, 5 }, new double[0], 6);
+		}
+
+		[TestMethod]
+		public void EgyptTriangleTwoSides() {
+			TestAlgorithm(new double[] { 3, 4 }, new double[] { 90f }, 6);
+		}
+
+		[TestMethod]
+		public void EgyptTriangleOneSide() {
+			TestAlgorithm(new double[] { 3 }, new double[] { 90f, 53.13 }, 6);
+		}
+
+		
+
+
+
+	}
+}
---
+++ Triangle_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_01 {
+
+	class Program {
+
+		static void Main(string[] args) {
+			//Triangle t = new Triangle();
+			//Console.WriteLine(t.GetArea());
+			//Console.ReadKey();
+		}
+
+	}
+
+}
